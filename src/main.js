1f29e20da9999de508a7be8eb25c62f769a51fe2
Francisco Berti da Cruz <fbc991@live.com>
2023-02-02 22:42:47 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll("[data-tab-button]")
     const questions = document.querySelectorAll("[data-faq-question]")
 
+    const heroSection = document.querySelector(".hero")
+    const alturaHero = heroSection.clientHeight
 
+    window.addEventListener("scroll", function () {
+        const posicaoAtual = window.scrollY
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader()
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    //seção de atração, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function (button) {
             const abaAlvo = button.target.dataset.tabButton
@@ -14,10 +27,22 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
+    //seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener("click", abreOuFechaResposta)
     }
 })
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector("header")
+    header.classList.remove("header--is-hidden")
+
+}
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector("header")
+    header.classList.add("header--is-hidden")
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = "faq__questions__item--is-open"
